a4e5229ec21dfab248da03f6175e66885735b751
dpt2310 <dpt2310@me.com>
2023-06-16 16:06:04 -0400

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,15 +1,40 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = days[hours.getHours()];
+  if (hours < 10) {
+    minutes = `0${minutes}`;
+  }
+  let minutes = date.getMinutes;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("temperature");
   let cityElement = document.querySelector("city");
   let descriptionElement = document.querySelector("description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.wind.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "894e138c2b76b790b5e762ad5f9bcd4c";
