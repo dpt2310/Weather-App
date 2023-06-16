4bbe3d5c3bdc11667a6b3f93ddc8814b7a07e4a8
dpt2310 <dpt2310@me.com>
2023-06-16 15:32:01 -0400

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -9,11 +9,10 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "894e138c2b76b790b5e762ad5f9bcd4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
