38ba1952285ee25bb05418642414c21cc9640f5b
dpt2310 <dpt2310@me.com>
2023-06-30 18:39:13 -0400

    Displayed the forecast data

--- src/app.js
+++ src/app.js
@@ -21,28 +21,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHtml = `<div class="row">`;
-  let days = ["MON", "TUE", "WED", "THUR", "FRI", "SAT"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        ` 
               <div class="col-2">
-                <div class="weather-forecaste-date">${day}</div>
+                <div class="weather-forecaste-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://openweathermap.org/img/wn/03d@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }03d@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">29°</span>
-                  <span class="weather-forecast-temperature-min">21°</span>
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
               </div>
             `;
+    }
   });
 
   forecastHtml = forecastHtml + `</div>`;
